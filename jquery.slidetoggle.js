c771b479e0be38c8c0eba7de76186c96e10a1604
Hashrocket Workstation <dev@hashrocket.com>
2013-10-02 15:39:28 -0400

    initial commit

---
+++ jquery.slidetoggle.js
@@ -0,0 +1,36 @@
+(function($) {
+  $.fn.slidetoggle = function(options) {
+    options = options || {};
+
+    var className = options.className || 'slidetoggle'
+      , checkedText = options.checkedText || 'yes'
+      , uncheckedText = options.uncheckedText || 'no'
+      , $toggleTemplate = $('<div class="' + className + '"><span></span><div></div></div>');
+
+    function toggleText(checked) {
+      return checked ? checkedText : uncheckedText
+    }
+
+    $(document).on('click', 'div.' + className, function() {
+      var $toggle = $(this);
+      $toggle.prev().attr('checked', function(i, attr) {
+        $toggle.toggleClass('checked', attr).find('span').text(function() {
+          return toggleText(!attr);
+        });
+        return !attr;
+      });
+    });
+
+    return this.each(function() {
+      var $toggle = $toggleTemplate.clone();
+      $(this).hide()
+        .attr('checked', function(i, attr) {
+          $toggle.toggleClass('checked', attr).find('span').text(function() {
+            return toggleText(attr);
+          });
+          return attr;
+        })
+        .after($toggle);
+    });
+  }
+})(jQuery);
