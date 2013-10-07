fe9e75b16583ffe28af933dc95c68f1037737f19
Michael Joyce <engineering+mike@appstack.com>
2013-10-07 15:38:57 -0400

    refactor with settings

--- jquery.slidetoggle.js
+++ jquery.slidetoggle.js
@@ -1,26 +1,29 @@
 (function($) {
   $.fn.slidetoggle = function(options) {
-    options = options || {};
 
-    var className = options.className || 'slidetoggle'
-      , eventName = className + ':click'
-      , checkedText = options.checkedText || 'yes'
-      , uncheckedText = options.uncheckedText || 'no'
-      , $toggleTemplate = $('<div><div></div><div></div></div>').addClass(className);
+    var settings = $.extend({
+        className:     'slidetoggle',
+        checkedText:   'yes',
+        uncheckedText: 'no'
+      }, options || {})
+      , eventName = settings.className + ':click'
+      , $toggleTemplate = $('<div class="' +
+          settings.className + '"><div></div><div></div></div>');
 
     function toggleText(checked) {
-      return checked ? checkedText : uncheckedText
+      return checked ? settings.checkedText : settings.uncheckedText
     }
 
     $(document).off(eventName).on(eventName, function(e) {
       var $toggle = $(e.target);
       $toggle.prev().attr('checked', function(i, attr) {
-        $toggle.toggleClass('checked', attr).find(':first-child').text(toggleText(!attr));
+        $toggle.toggleClass('checked', attr)
+          .find(':first-child').text(toggleText(!attr));
         return !attr;
       });
     });
 
-    $(document).on('click', 'div.' + className, function() {
+    $(document).on('click', 'div.' + settings.className, function() {
       $(this).trigger(eventName);
     });
 
@@ -36,7 +39,8 @@
         .attr('checked', function(i, attr) {
           attr = attr || false;
           $toggle.attr('for', $checkbox.attr('id'))
-            .toggleClass('checked', attr).find(':first-child').text(toggleText(attr));
+            .toggleClass('checked', attr)
+              .find(':first-child').text(toggleText(attr));
           return attr;
         })
         .after($toggle);
