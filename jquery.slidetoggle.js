911d5f8714f962bf94dd61aa2c3264f5195eb1ad
Michael Joyce <engineering+mike@appstack.com>
2013-10-07 16:10:51 -0400

    optimize, demo page

--- jquery.slidetoggle.js
+++ jquery.slidetoggle.js
@@ -7,29 +7,31 @@
         uncheckedText: 'no'
       }, options || {})
       , eventName = settings.className + ':click'
+      , textToggle = [settings.uncheckedText, settings.checkedText]
       , $toggleTemplate = $('<div class="' +
           settings.className + '"><div></div><div></div></div>');
 
-    function toggleText(checked) {
-      return checked ? settings.checkedText : settings.uncheckedText
+    if(!$.fn.slidetoggle.init) {
+
+      $(document).on(eventName, function(e) {
+        var $toggle = $(e.target);
+        $toggle.prev().attr('checked', function(i, attr) {
+          $toggle.toggleClass('checked', attr)
+            .find(':first-child').text(textToggle[!attr-0]);
+          return !attr;
+        });
+      });
+
+      $(document).on('click', 'div.' + settings.className, function() {
+        $(this).trigger(eventName);
+      });
+
+      $(document).on('click', 'label', function() {
+        $('div[for=' + $(this).attr('for') + ']').click();
+      });
     }
 
-    $(document).off(eventName).on(eventName, function(e) {
-      var $toggle = $(e.target);
-      $toggle.prev().attr('checked', function(i, attr) {
-        $toggle.toggleClass('checked', attr)
-          .find(':first-child').text(toggleText(!attr));
-        return !attr;
-      });
-    });
-
-    $(document).on('click', 'div.' + settings.className, function() {
-      $(this).trigger(eventName);
-    });
-
-    $(document).on('click', 'label', function() {
-      $('div[for=' + $(this).attr('for') + ']').click();
-    });
+    $.fn.slidetoggle.init = true;
 
     return this.each(function() {
       var $checkbox = $(this)
@@ -38,9 +40,10 @@
       $checkbox.hide()
         .attr('checked', function(i, attr) {
           attr = attr || false;
+          if(attr == 'checked') attr = true;
           $toggle.attr('for', $checkbox.attr('id'))
             .toggleClass('checked', attr)
-              .find(':first-child').text(toggleText(attr));
+              .find(':first-child').text(textToggle[attr-0]);
           return attr;
         })
         .after($toggle);
