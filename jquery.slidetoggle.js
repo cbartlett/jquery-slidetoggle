16fc24951b4a89168a80422e5143710870585e15
Michael Joyce <engineering+mike@appstack.com>
2013-10-07 14:53:58 -0400

    optimize everything

--- jquery.slidetoggle.js
+++ jquery.slidetoggle.js
@@ -5,12 +5,7 @@
     var className = options.className || 'slidetoggle'
       , checkedText = options.checkedText || 'yes'
       , uncheckedText = options.uncheckedText || 'no'
-      , $toggleTemplate = $([
-        '<div class="' + className + '">',
-        '<div class="' + className + '-text'  + '"></div>',
-        '<div class="' + className + '-slider' + '"></div>',
-        '</div>'
-      ].join(''));
+      , $toggleTemplate = $('<div><div></div><div></div></div>').addClass(className);
 
     function toggleText(checked) {
       return checked ? checkedText : uncheckedText
@@ -24,12 +19,19 @@
       });
     });
 
+    $(document).on('click', 'label', function() {
+      $('div[for=' + $(this).attr('for') + ']').click();
+    });
+
     return this.each(function() {
-      var $toggle = $toggleTemplate.clone();
-      $(this).hide()
+      var $checkbox = $(this)
+        , $toggle = $toggleTemplate.clone();
+
+      $checkbox.hide()
         .attr('checked', function(i, attr) {
           attr = attr || false;
-          $toggle.toggleClass('checked', attr).find(':first-child').text(toggleText(attr));
+          $toggle.attr('for', $checkbox.attr('id'))
+            .toggleClass('checked', attr).find(':first-child').text(toggleText(attr));
           return attr;
         })
         .after($toggle);
